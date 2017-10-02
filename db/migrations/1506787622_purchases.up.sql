bd7ee9b951f38f1eac5d1c12f5c16eb6c0f23fb6
mtdx <ms2asdk@gmail.com>
2017-10-02 05:37:05 -0700

    Purchases table

--- db/migrations/1506787622_purchases.up.sql
+++ db/migrations/1506787622_purchases.up.sql
@@ -7,7 +7,24 @@ CREATE TABLE purchases
   user_steam_id     BIGINT                                 NOT NULL
     CONSTRAINT purchases_user_steam_id_foreign
     REFERENCES users,
+  tradeoffer_id     BIGINT                                 NOT NULL
+    CONSTRAINT purchases_tradeoffer_id_foreign
+    REFERENCES tradeoffers,   
+  status            VARCHAR(25)                            NOT NULL,  
+  type              VARCHAR(25)                            NOT NULL,
+  amount            INTEGER                                NOT NULL,
+  unit_price        NUMERIC(10, 2)                         NOT NULL,
+  payment_address   VARCHAR(60),
+  usd_rate          NUMERIC(10, 2)                         NOT NULL,
+  currency          VARCHAR(3)                             NOT NULL,
+  usd_total         NUMERIC(10, 2)                         NOT NULL,
+  crypto_total      NUMERIC(10, 2)                         NOT NULL,
+  paid_to           VARCHAR(7)                            NOT NULL,
   created_at        TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
 CREATE INDEX purchases_user_steam_id_index
   ON purchases (user_steam_id);
+CREATE INDEX purchases_tradeoffer_id_index
+  ON purchases (tradeoffer_id);  
+CREATE INDEX purchases_payment_address_index
+  ON purchases (payment_address);  
