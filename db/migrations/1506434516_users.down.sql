39cf42da96f1e97565c18d27c1208b22934667fa
mtdx <ms2asdk@gmail.com>
2017-10-02 09:43:02 -0700

    update users stats

--- db/migrations/1506434516_users.down.sql
+++ db/migrations/1506434516_users.down.sql
@@ -1,1 +1,3 @@
 DROP TABLE users;
+DROP TRIGGER stats_users_nr_increment ON users;
+DROP FUNCTION stats_users_nr_increment();
--- db/migrations/1506434516_users.up.sql
+++ db/migrations/1506434516_users.up.sql
@@ -1,12 +1,26 @@
 CREATE TABLE users
 (
-  steam_id  BIGINT NOT NULL
+  steam_id        BIGINT             NOT NULL
     CONSTRAINT users_pkey
     PRIMARY KEY,
-  trade_link_url VARCHAR(120),
+  trade_link_url  VARCHAR(120),
   bitcoin_balance INTEGER DEFAULT 0  NOT NULL,
   csgokey_balance INTEGER DEFAULT 0  NOT NULL,
-  display_name  VARCHAR(40) NOT NULL,
-  avatar        VARCHAR(255) NOT NULL,
-  added_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+  display_name    VARCHAR(40)        NOT NULL,
+  avatar          VARCHAR(255)       NOT NULL,
+  added_at        TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
+
+CREATE OR REPLACE FUNCTION stats_users_nr_increment()
+  RETURNS TRIGGER AS
+$func$
+BEGIN
+  UPDATE stats SET users_nr = users_nr + 1;
+  RETURN NEW;
+END;
+$func$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER stats_users_nr_increment
+AFTER INSERT ON users FOR EACH ROW
+EXECUTE PROCEDURE stats_users_nr_increment()
