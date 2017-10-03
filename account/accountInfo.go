b79bd61a02d152901eba53efbf297ba7d1bc5cc4
mtdx <ms2asdk@gmail.com>
2017-10-03 12:35:04 -0700

    validate response

--- account/accountInfo.go
+++ account/accountInfo.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-chi/jwtauth"
 	"github.com/go-chi/render"
 	"github.com/mtdx/keyc/common"
+	"github.com/mtdx/keyc/validator"
 )
 
 // InfoResponse ...
@@ -22,17 +23,22 @@ func (rd *InfoResponse) Render(w http.ResponseWriter, r *http.Request) error {
 
 // InfoHandler rest route handler
 func InfoHandler(w http.ResponseWriter, r *http.Request) {
-	info := &InfoResponse{}
+	inforesp := &InfoResponse{}
 	_, claims, _ := jwtauth.FromContext(r.Context())
 	dbconn := r.Context().Value("DBCONN").(*sql.DB)
 	err := dbconn.QueryRow(`SELECT bitcoin_balance, csgokey_balance, trade_link_url 
-	FROM users WHERE steam_id = $1`, claims["id"]).Scan(&info.BitcoinBalance,
-		&info.CsgokeyBalance, &info.TradeLinkURL)
+	FROM users WHERE steam_id = $1`, claims["id"]).Scan(&inforesp.BitcoinBalance,
+		&inforesp.CsgokeyBalance, &inforesp.TradeLinkURL)
 	if err != nil || err == sql.ErrNoRows {
 		render.Render(w, r, common.ErrInternalServer(err))
 		return
 	}
 
+	if err := validator.Validate(inforesp); err != nil {
+		render.Render(w, r, common.ErrInternalServer(err))
+		return
+	}
+
 	render.Status(r, http.StatusOK)
-	render.Render(w, r, info)
+	render.Render(w, r, inforesp)
 }
--- account/purchases.go
+++ account/purchases.go
@@ -7,16 +7,18 @@ import (
 	"github.com/go-chi/jwtauth"
 	"github.com/go-chi/render"
 	"github.com/mtdx/keyc/common"
+	"github.com/mtdx/keyc/validator"
 )
 
 // PurchasesResponse ...
 type PurchasesResponse struct {
-	Type           string  `json:"type" validate:"nonzero"`
 	Status         string  `json:"status" validate:"nonzero"`
+	Type           string  `json:"type" validate:"nonzero"`
 	Amount         uint32  `json:"amount" validate:"min=1"`
-	UnitPrice      uint32  `json:"unit_price" validate:"nonzero"`
+	UnitPrice      float64 `json:"unit_price" validate:"nonzero"`
 	PaymentAddress string  `json:"payment_address" validate:"nonzero"`
 	USDRate        float64 `json:"usd_rate" validate:"nonzero"`
+	Currency       string  `json:"currency" validate:"len=3"`
 	USDTotal       float64 `json:"usd_total" validate:"nonzero"`
 	CryptoTotal    float64 `json:"crypto_total" validate:"nonzero"`
 	CreatedAt      string  `json:"created_at" validate:"nonzero"`
@@ -30,29 +32,37 @@ func (rd *PurchasesResponse) Render(w http.ResponseWriter, r *http.Request) erro
 func PurchasesHandler(w http.ResponseWriter, r *http.Request) {
 	_, claims, _ := jwtauth.FromContext(r.Context())
 	dbconn := r.Context().Value("DBCONN").(*sql.DB)
-	rows, err := dbconn.Query(`SELECT type, status, failure_details, amount, created_at 
-		FROM Purchases WHERE user_steam_id = $1`, claims["id"])
+	rows, err := dbconn.Query(`SELECT status, type, amount, unit_price, payment_address, usd_rate, 
+		currency, usd_total, crypto_total, created_at FROM purchases WHERE user_steam_id = $1`,
+		claims["id"])
 	if err != nil {
 		render.Render(w, r, common.ErrInternalServer(err))
 		return
 	}
 	defer rows.Close()
-	list := []render.Renderer{}
+	purchasesresp := []render.Renderer{}
 	for rows.Next() {
 		resp := &PurchasesResponse{}
-		if err := rows.Scan(&resp.Type, &resp.Status, &resp.FailureDetails, &resp.Amount, &resp.CreatedAt); err != nil {
+		if err := rows.Scan(&resp.Status, &resp.Type, &resp.Amount, &resp.UnitPrice, &resp.PaymentAddress,
+			&resp.USDRate, &resp.Currency, &resp.USDTotal, &resp.CryptoTotal, &resp.CreatedAt); err != nil {
 			render.Render(w, r, common.ErrInternalServer(err))
 			return
 		}
-		list = append(list, resp)
+		purchasesresp = append(purchasesresp, resp)
 	}
 	if err := rows.Err(); err != nil {
 		render.Render(w, r, common.ErrInternalServer(err))
 		return
 	}
 
+	for _, purchase := range purchasesresp {
+		if err := validator.Validate(purchase); err != nil {
+			render.Render(w, r, common.ErrInternalServer(err))
+		}
+	}
+
 	render.Status(r, http.StatusOK)
-	if err := render.RenderList(w, r, list); err != nil {
+	if err := render.RenderList(w, r, purchasesresp); err != nil {
 		render.Render(w, r, common.ErrRender(err))
 	}
 }
