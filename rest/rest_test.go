d012b6d44cb0e9647b98317368d5684cf33ca5f7
mtdx <ms2asdk@gmail.com>
2017-10-03 13:29:29 -0700

    fix routes and insert purchases

--- rest/rest_test.go
+++ rest/rest_test.go
@@ -61,13 +61,13 @@ func TestAccountSummary(t *testing.T) {
 }
 
 func TestTradeoffersAuth(t *testing.T) {
-	common.AssertAuth(t, ts, "GET", "/api/v1/tradeoffers")
+	common.AssertAuth(t, ts, "GET", "/api/v1/account/tradeoffers")
 }
 
 func TestTradeoffers(t *testing.T) {
 	t.Parallel()
 
-	_, body = common.TestRequest(t, ts, "GET", "/api/v1/tradeoffers", nil, jwt)
+	_, body = common.TestRequest(t, ts, "GET", "/api/v1/account/tradeoffers", nil, jwt)
 	tradeoffersresp := make([]account.TradeoffersResponse, 2)
 	if err := json.Unmarshal([]byte(body), &tradeoffersresp); err != nil {
 		t.Fatalf("Failed to Unmarshal, got: %s, error: %s", body, err.Error())
@@ -100,10 +100,19 @@ func setupTestUserData(dbconn *sql.DB) string {
 		os.Exit(1)
 	}
 
-	dbconn.Exec(`INSERT INTO tradeoffers (user_steam_id, type, status, failure_details, amount) 
-	 VALUES ($1, $2, $3, $4, $5)`, testSteamID, account.CSGO_KEY, account.ACTIVE, "failuredetails1", 1)
-	dbconn.Exec(`INSERT INTO tradeoffers (user_steam_id, type, status, failure_details, amount) 
-	 VALUES ($1, $2, $3, $4, $5)`, testSteamID, account.CSGO_CASE, account.ACCEPTED, "failuredetails2", 2)
+	dbconn.Exec(`INSERT INTO tradeoffers (id, user_steam_id, type, status, failure_details, amount) 
+	 VALUES (1, $1, $2, $3, $4, $5)`, testSteamID, account.CSGO_KEY, account.ACTIVE, "failuredetails1", 1)
+	dbconn.Exec(`INSERT INTO tradeoffers (id, user_steam_id, type, status, failure_details, amount) 
+	 VALUES (2, $1, $2, $3, $4, $5)`, testSteamID, account.CSGO_CASE, account.ACCEPTED, "failuredetails2", 2)
+
+	dbconn.Exec(`INSERT INTO purchases (id, user_steam_id, tradeoffer_id, status, type, amount, unit_price, 
+		payment_address, usd_rate, currency, usd_total, crypto_total) 
+	 VALUES (1, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`, testSteamID, 1, account.UNPAID, account.CSGO_KEY,
+		1, 1.86, "13XrFK2m8tXvM5srR9tFPYsm2mpmRyAnXb", 4.3343, account.BTC, 200, 0.00425076)
+	dbconn.Exec(`INSERT INTO purchases (id, user_steam_id, tradeoffer_id, status, type, amount, unit_price, 
+			payment_address, usd_rate, currency, usd_total, crypto_total) 
+		 VALUES (2, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`, testSteamID, 2, account.UNPAID, account.CSGO_CASE,
+		2, 1.92, "13XrFK2m8tXvM5srR9tFPYsm2mpmRyAnXb", 5.2212, account.BTC, 200, 0.00568021)
 
 	return jwt
 }
